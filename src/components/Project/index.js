4a5339bdee0e9bbe597697b1c95a182ed54dc1a5
LChinQuee <81491306+lchinquee@users.noreply.github.com>
2021-09-06 16:57:48 -0400

    add projects, finish styling

--- src/components/Project/index.js
+++ src/components/Project/index.js
@@ -2,86 +2,87 @@ import React from 'react';
 
 function Project() {
     return (
-        <section>
+        <section id="work" className="my-work">
             <div className="work-title">
                 <h2 className="section-title">
-                    Project List
+                    Projects
                 </h2>
             </div>
-            <div className="flex-row">
+            <div className="flex-work">
                 <div className="rb-content">
-                    <h3>
-                        {/* What I Had For Dinner Project 2 */}
-                        <a href="https://whatihadfordinner.herokuapp.com/">What I Had For Dinner</a>
-                        <a href="https://github.com/lchinquee/whatihadfordinner">GitHub Link</a>
-                    </h3>
-                    <p>
-                        CSS/Animate/JavaScript/Node/Express
-                    </p>
-                </div>
-                <div className="led-content">
                     <div>
                         <h3>
-                            {/* What's For Dinner Project 1 */}
-                            <a href="https://jilliana328.github.io/whats-for-dinner/">What's For Dinner</a>
-                            <a href="https://github.com/lchinquee/whats-for-dinner">GitHub Link</a>
+                            {/* <!--What I Had For Dinner Project 2--> */}
+                            <a href="https://whatihadfordinner.herokuapp.com/">What I Had For Dinner</a>
+                            <a href="https://github.com/lchinquee/whatihadfordinner">GitHub</a>
                         </h3>
                         <p>
-                            HTML/CSS/JavaScript/JQuery
+                            CSS/Animate/JavaScript/Node/Express
                         </p>
-                    </div>
+                    </div>                    
                 </div>
-                <div className="flex-row">
-                    <div className="calc-content">
+                <div className="wd-content">
                         <div>
                             <h3>
-                                {/* Coding Quiz Challenge */}
+                                {/* <!--Includes link to Run Buddy project--> */}
+                                <a href="https://jilliana328.github.io/whats-for-dinner/">What's For Dinner</a>
+                                <a href="https://github.com/lchinquee/whats-for-dinner">GitHub</a>
+                            </h3>
+                            <p>
+                                HTML/CSS/JavaScript/JQuery/Bulma
+                            </p>
+                        </div>                        
+                    </div>
+                <div className="flex-row">
+                    <div className="led-content">
+                        <div>
+                            <h3>
+                                {/* <!--Coding Quiz Challenge--> */}
                                 <a href="https://lchinquee.github.io/nextreams/">Coding Quiz</a>
-                                <a href="https://github.com/lchinquee/nextreams">GitHub Link</a>
+                                <a href="https://github.com/lchinquee/nextreams">GitHub</a>
                             </h3>
                             <p>
                                 HTML/CSS/JavaScript
                             </p>
-                        </div>
+                        </div>                        
+                    </div>   
+                    <div className="calc-content">
+                        <div>
+                            <h3>
+                                {/* <!--ReadingList Website--> */}
+                                <a href="https://calm-depths-97144.herokuapp.com/">ReadingList</a>
+                                <a href="https://github.com/lchinquee/slatorer">GitHub</a>
+                            </h3>
+                            <p>
+                                JavaScript/Node/Express/Handlebars/MySQL2/Seqeulize
+                            </p>
+                        </div>                        
                     </div>
+                </div>   
+                <div className="flex-row">
                     <div className="pzl-content">
                         <div>
                             <h3>
-                                {/* ReadingList App - MVP Challenge */}
-                                <a href="https://calm-depths-97144.herokuapp.com/">ReadingList App</a>
-                                <a href="https://github.com/lchinquee/slatorer">GitHub Link</a>
+                                {/* <!--Daily Schedule Planner--> */}
+                                <a href="https://lchinquee.github.io/humorite/">Daily Schedule Planner</a>
+                                <a href="https://github.com/lchinquee/humorite">GitHub</a>
+                            </h3>
+                            <p>
+                                HTML/CSS/JavaScript/JQuery/Bootstrap
+                            </p>
+                        </div>                        
+                    </div>   
+                    <div className="surf-content">
+                        <div>
+                            <h3>
+                                {/* <!--Includes link to Run Buddy project--> */}
+                                <a href="https://lchinquee.github.io/run-buddy/">Run Buddy</a>
+                                <a href="https://github.com/lchinquee/run-buddy">GitHub</a>
                             </h3>
                             <p>
                                 HTML/CSS
                             </p>
-                        </div>
-                    </div>
-                    <div className="flex-row">
-                        <div className="surf-content">
-                            <div>
-                                <h3>
-                                    {/* Run Buddy In-Class Project */}
-                                    <a href="https://lchinquee.github.io/run-buddy/">Run Buddy</a>
-                                    <a href="https://github.com/lchinquee/run-buddy">GitHub Link</a>
-                                </h3>
-                                <p>
-                                    HTML/CSS
-                                </p>
-                            </div>
-
-                        </div>
-                        <div className="surf-content">
-                            <div>
-                                <h3>
-                                    {/* Work Day Scheduler Challenge */}
-                                    <a href="https://github.com/lchinquee/humorite">Work Day Scheduler</a>
-                                    <a href="https://lchinquee.github.io/humorite/">GitHub Link</a>
-                                </h3>
-                                <p>
-                                    React/JavaScript/CSS
-                                </p>
-                            </div>
-                        </div>
+                        </div>                        
                     </div>
                 </div>
             </div>
