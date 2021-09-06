5cce54dda0450d3b0f850cb946def090fa9c1576
LChinQuee <81491306+lchinquee@users.noreply.github.com>
2021-09-06 15:28:28 -0400

    initial styling

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -4,8 +4,14 @@ import coverImage from '../../assets/cover/profile-pic.jpg';
 function About() {
   return (
     <section>
-      <img src={coverImage} className="my-2" style={{ width: "50%" }} alt="cover" />
-      <p>Leslie ChinQuee is a Systems Engineer II at Sterling Medical Devices. Currently, working on device design and development, actively learning about the software side of the process.</p>
+      <container>
+        <div className="flex-row">
+          <img src={coverImage} className="my-2" style={{ width: "50%" }} alt="cover" />
+        </div>
+        <div className="flex-row">
+          <p>Leslie ChinQuee is a Systems Engineer II at Sterling Medical Devices. Currently, working on device design and development, actively learning about the software side of the process.</p>
+        </div>
+      </container>    
     </section>
   );
 }
--- src/components/Resume/index.js
+++ src/components/Resume/index.js
@@ -8,14 +8,16 @@ function Resume() {
             <a href="https://github.com/lchinquee/xregulargo/files/7108588/resume.pdf">Leslie ChinQuee's Resume</a>
         </div>
         <h2>List of Proficiencies</h2>
-        <ul>
-            <li>
-                HTML, CSS, JavaScript
-            </li>
-            <li>
-                Node, Express, React, MySQL, MongoDB
-            </li>
-        </ul>
+        <div className="proficiencies">
+            <ul>
+                <li>
+                    HTML, CSS, JavaScript
+                </li>
+                <li>
+                    Node, Express, React, MySQL, MongoDB
+                </li>
+            </ul>
+        </div>
     </section>
   );
 }
